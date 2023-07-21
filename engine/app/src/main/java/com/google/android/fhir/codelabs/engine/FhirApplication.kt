e695317c454385c77a34d6e42b0ecbf30b3a5e25
joe052 <joekaranjasenior52@gmail.com>
2023-07-21 23:06:04 +0300

    message

--- engine/app/src/main/java/com/google/android/fhir/codelabs/engine/FhirApplication.kt
+++ engine/app/src/main/java/com/google/android/fhir/codelabs/engine/FhirApplication.kt
@@ -39,7 +39,7 @@ class FhirApplication : Application() {
         RECREATE_AT_OPEN,
         ServerConfiguration(
 //          baseUrl = "http://10.0.2.2:8080/fhir/",
-          baseUrl = "https://2c21-102-68-76-201.ngrok-free.app/fhir/",
+          baseUrl = "https://7cdb-102-68-76-201.ngrok-free.app/fhir/",
           httpLogger =
             HttpLogger(
               HttpLogger.Configuration(
